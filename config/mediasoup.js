63300aa5cb03b1d3c2cb9b6c9972a6941cf646df
adtjha <adtjha@hotmail.com>
2021-12-28 15:46:54 +0000

    local ip

--- config/mediasoup.js
+++ config/mediasoup.js
@@ -1,27 +1,4 @@
-import os from "os";
 
-const ifaces = os.networkInterfaces();
-
-const getLocalIp = () => {
-    let localIp = "127.0.0.1";
-
-    Object.keys(ifaces).forEach((ifname) => {
-        for (const iface of ifaces[ifname] ?? []) {
-            // Ignore IPv6 and 127.0.0.1
-            if (iface.family !== "IPv4" || iface.internal !== false) {
-                continue;
-            }
-
-            // Set the local ip to the first IPv4 address found and exit the loop
-            localIp = iface.address;
-            return;
-        }
-    });
-
-    return localIp;
-};
-
-const localIp = getLocalIp();
 
 const mediaCodecs = {
     video: [
--- index.js
+++ index.js
@@ -26,6 +26,28 @@ server.listen(PORT, () => {
 const mediasoup = require("mediasoup");
 process.env.DEBUG = "mediasoup*";
 
+import os from "os";
+
+const ifaces = os.networkInterfaces();
+global.localIp = "127.0.0.1";
+
+const getLocalIp = () => {
+  Object.keys(ifaces).forEach((ifname) => {
+    for (const iface of ifaces[ifname] ?? []) {
+      // Ignore IPv6 and 127.0.0.1
+      if (iface.family !== "IPv4" || iface.internal !== false) {
+        continue;
+      }
+
+      // Set the local ip to the first IPv4 address found and exit the loop
+      localIp = iface.address;
+      return;
+    }
+  });
+};
+
+const localIp = getLocalIp();
+
 global.worker = {};
 global.rooms = [];
 
