9e10459d8c79accbfdb8d3a82c4aea0a5c93e769
adtjha <adtjha@hotmail.com>
2022-01-23 04:30:58 +0530

    async

--- index.js
+++ index.js
@@ -95,20 +95,22 @@ require("./src/socket")(io);
 mediasoup.observer.on("newworker", (worker) => {
   console.log("new worker created [worke.pid:%d]", worker.pid);
 
-  try {
-    const timeseries = await readTimeSeriesFields(moniteringClient)
-    timeseries.length > 1 && timeseries.forEach(e => {
-      e.points.forEach(p => {
-        if (JSON.stringify(p.value) !== '0') {
-          localConsumerCount = p.value
-        }
-      })
-    });
-  } catch (error) {
-    await createMetricDescriptor(moniteringClient)
-  } finally {
-    await writeTimeSeriesData(localConsumerCount, moniteringClient);
-  }
+  (async () => {
+    try {
+      const timeseries = await readTimeSeriesFields(moniteringClient)
+      timeseries.length > 1 && timeseries.forEach(e => {
+        e.points.forEach(p => {
+          if (JSON.stringify(p.value) !== '0') {
+            localConsumerCount = p.value
+          }
+        })
+      });
+    } catch (error) {
+      await createMetricDescriptor(moniteringClient)
+    } finally {
+      await writeTimeSeriesData(localConsumerCount, moniteringClient);
+    }
+  })()
 
   worker.observer.on("close", () => {
     console.log("worker closed [worker.pid:%d]", worker.pid);
